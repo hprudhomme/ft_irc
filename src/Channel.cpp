61a3d6e68eeacc764ee0059ca20e81bc5e5bce36
Hadrien Prudhomme <hprudhom@z2r7p4.42lyon.fr>
2022-10-21 17:11:41 +0200

    remove client from chan

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -22,4 +22,27 @@ void						Channel::broadcast_channel(std::string const &message) const
 	_server->broadcast(message);
 }
 
+void Channel::removeClient(Client *client)
+{
+	_oper_clients.erase(std::remove(_oper_clients.begin(), _oper_clients.end(), client), _oper_clients.end());
+	_clients.erase(std::remove(_clients.begin(), _clients.end(), client), _clients.end());
+	client->leave(this);
+
+	if (_clients.empty())
+	{
+		// free chan and remove it from server
+		return;
+	}
+
+	if (_admin == client)
+		_admin = _clients.begin().operator*();
+
+	// message to say that there is a new admin
+}
+
+void Channel::removeOper(Client *client)
+{
+	_oper_clients.erase(std::remove(_oper_clients.begin(), _oper_clients.end(), client), _oper_clients.end());
+}
+
 
